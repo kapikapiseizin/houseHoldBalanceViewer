91329b45481c3b818c6edecbe9fa32e09ebcb9e7
kapikapiseizin <ice1black0927@gmail.com>
2026-03-16 14:26:40 +0900

    AccessSheetの画面遷移

--- household-balance-viewer/src/AccessSheet.tsx
+++ household-balance-viewer/src/AccessSheet.tsx
@@ -1,12 +1,83 @@
+import { useState } from 'react';
+
 type AccessSheetProps = {
   accessToken: string;
-  onSuccess: (spreadsheetId: string) => void;
+  onSuccess: (spreadSheetID: string | undefined) => void;
 };
 
+type Phase = 'selectMode' | 'createSheet' | 'selectSheet';
+
 export default function AccessSheet({ accessToken, onSuccess }: AccessSheetProps) {
+  const [phase, setPhase] = useState<Phase>('selectMode');
+
+  if (phase === 'selectMode') {
+    return (
+      <SelectMode
+        accessToken={accessToken}
+        onChoiceCreateSheet={() => setPhase('createSheet')}
+        onChoiceSelectSheet={() => setPhase('selectSheet')}
+      />
+    );
+  }
+
+  if (phase === 'createSheet') {
+    return (
+      <CreateSheet
+        accessToken={accessToken}
+        onCreate={(spreadSheetID) => onSuccess(spreadSheetID)}
+      />
+    );
+  }
+
+  if (phase === 'selectSheet') {
+    return (
+      <SelectSheet
+        accessToken={accessToken}
+        onSelect={(spreadSheetID) => onSuccess(spreadSheetID)}
+      />
+    );
+  }
+
+  return null;
+}
+
+type SelectModeProps = {
+  accessToken: string;
+  onChoiceCreateSheet: () => void;
+  onChoiceSelectSheet: () => void;
+};
+
+function SelectMode({ accessToken, onChoiceCreateSheet, onChoiceSelectSheet }: SelectModeProps) {
   return (
     <div>
-      <button onClick={() => onSuccess("")}>Access Sheet</button>
+      <button onClick={onChoiceCreateSheet}>Create</button>
+      <button onClick={onChoiceSelectSheet}>Select</button>
+    </div>
+  );
+}
+
+type CreateSheetProps = {
+  accessToken: string;
+  onCreate: (spreadSheetID: string | undefined) => void;
+};
+
+function CreateSheet({ accessToken, onCreate }: CreateSheetProps) {
+  return (
+    <div>
+      <button onClick={() => onCreate(undefined)}>Create</button>
+    </div>
+  );
+}
+
+type SelectSheetProps = {
+  accessToken: string;
+  onSelect: (spreadSheetID: string | undefined) => void;
+};
+
+function SelectSheet({ accessToken, onSelect }: SelectSheetProps) {
+  return (
+    <div>
+      <button onClick={() => onSelect(undefined)}>Select</button>
     </div>
   );
 }
